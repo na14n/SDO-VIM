35e621da39d2b8157565ffe82d3b33da03457011
inicolas <antonio.iannicolas@gmail.com>
2024-08-17 00:25:48 +0800

    Updated Coordinator View

---
+++ views/coordinator/create.view.php
@@ -0,0 +1,12 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/sidebar.php') ?>
+
+
+<!-- Your HTML code goes here -->
+
+<main class="main">
+
+    Welcome to Coordinator Dashboard
+</main>
+
+<?php require base_path('views/partials/footer.php') ?>
