e784f17c6daffcf610134bd88aeae072f16711ea
inicolas <na14an.07@gmail.com>
2024-08-23 07:34:07 +0800

    Created Approved Requests Route, Controller, and View

---
+++ Http/controllers/users/approved/index.php
@@ -0,0 +1,56 @@
+<?php
+
+//  ==========================================
+//           This is the Controller 
+// ===========================================
+// 
+//  This is where you load the corresponding
+//  view file for this route if available
+// 
+//   Use the view() function and feed the 
+//   full path of the view.
+// 
+//   Being the controller file. This is where 
+//   the data is get, manipulated, and/or
+//   saved.
+//      
+//   You can pass variables to your view as the
+//   second parameter of the view function.
+//      
+//   view('notes/{id}', ['notes' => $notes])
+//
+//   view variables are passed as keu-value
+//   pairs as illustrated in the example above.
+//
+
+use Core\Database;
+use Core\App;
+
+$db = App::resolve(Database::class);
+
+$users = [];
+
+$users = $db->query("
+    SELECT 
+        u.user_id,
+        u.school_id,
+        u.user_name,
+        u.date_added,
+        u.date_modified,
+        CASE
+            WHEN u.role = 1 THEN 'Coordinator'
+            WHEN u.role = 2 THEN 'Custodian'
+        END as role,
+        s.school_name AS school,
+        c.contact_name,
+        c.contact_no,
+        c.contact_email
+    FROM users u
+    JOIN schools s ON u.school_id = s.school_id
+    LEFT JOIN school_contacts c ON u.school_id = c.school_id
+")->get();
+
+view('users/approved/index.view.php', [
+    'heading' => 'Approved Requests',
+    'users' => $users
+]);
--- routes.php
+++ routes.php
@@ -41,6 +41,8 @@ $router->get('/coordinator/schools', 'schools/index.php')->only('coordinator');
 
 $router->get('/coordinator/users', 'users/index.php')->only('coordinator');
 $router->get('/coordinator/users/pending', 'users/pending/index.php')->only('coordinator');
+$router->get('/coordinator/users/approved', 'users/approved/index.php')->only('coordinator');
+$router->get('/coordinator/users/denied', 'users/denied/index.php')->only('coordinator');
 
 
 $router->get('/403', 'http_errors/403.php');
