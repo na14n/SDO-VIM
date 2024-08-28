696f7889ac6dbdf2d0c708eee32c8baf92109326
inicolas <na14an.07@gmail.com>
2024-08-28 22:19:29 +0800

    Updated AddEditSchoolForm to support all inputs

--- Http/Forms/AddEditSchoolForm.php
+++ Http/Forms/AddEditSchoolForm.php
@@ -49,12 +49,37 @@ class AddEditSchoolForm
     public function __construct(public array $attributes)
     {
 
-        if (!Validator::string($attributes['contact_no'], 11, 11)) {
-            $this->errors['contact_no'] = 'Please enter a valid contact number';
+        if (!Validator::regex($attributes['school_id'], '/^\d{6}/')) {
+            $this->errors['school_id'] = 'Please enter a valid 6-digit School ID.';
         }
 
-        if (!Validator::email($attributes['contact_email'], 1)) {
-            $this->errors['contact_email'] = 'Please enter a valid email address';
+        if (!Validator::string($attributes['school_name'], 1, 32)) {
+            $this->errors['school_name'] = 'Please enter a valid School Name.';
+        }
+
+        if (!Validator::regex($attributes['school_type'], '/^1|2/')) {
+            $this->errors['school_type'] = 'Please select a valid School Type.';
+        }
+
+        if (!Validator::regex($attributes['school_district'], '/^1|2|3|4/')) {
+            $this->errors['school_district'] = 'Please select a valid School District.';
+        }
+
+        if (!Validator::regex($attributes['school_division'], '/^1|2|3|4/')) {
+            $this->errors['school_division'] = 'Please select a valid School Division.';
+        }
+
+
+        if (!Validator::string($attributes['contact_name'], 1, 32)) {
+            $this->errors['contact_name'] = 'Please enter a valid Contact Name that is under 32 characters.';
+        }
+
+        if (!Validator::regex($attributes['contact_no'], '/^\d{11}/')) {
+            $this->errors['contact_no'] = 'Please enter a valid contact number.';
+        }
+
+        if (!Validator::email($attributes['contact_email'])) {
+            $this->errors['contact_email'] = 'Please enter a valid email address.';
         }
     }
 
