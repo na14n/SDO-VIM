1b0cfa61693c087c0be1f8dece635d29688841b1
inicolas <na14an.07@gmail.com>
2024-09-03 21:08:32 +0800

    Created Toast Handler in footer.php
    -- Flash notifications on 'notification'
    -- provide 'text' for the message, 'duration' for the duration, 'position' for the x alignment, 'bg' for the background css

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,4 +1,20 @@
         <script src="/js/bootstrap.bundle.min.js"></script>
-    </body>
-
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                if (<?php echo json_encode(isset($_SESSION['_flash']['notification']) &&  count($_SESSION['_flash']['notification']) > 0); ?>) {
+                    Toastify({
+                        text: "<?php echo $_SESSION['_flash']['notification']['text'] ?? '' ?>",
+                        duration: <?php echo $_SESSION['_flash']['notification']['duration'] ?? '' ?>,
+                        close: true,
+                        gravity: "bottom",
+                        position: "<?php echo $_SESSION['_flash']['notification']['position'] ?? '' ?>",
+                        backgroundColor: "<?php echo $_SESSION['_flash']['notification']['bg'] ?? '' ?>",
+                        stopOnFocus: true,
+                        offset: {
+                            y: '3rem'
+                        },
+                    }).showToast();
+                }
+            });          
+        </script>
 </html>
