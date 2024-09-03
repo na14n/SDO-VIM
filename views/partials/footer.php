98659cacb5ad2601c83be046177246ce2bbc0ffc
inicolas <na14an.07@gmail.com>
2024-09-03 21:08:59 +0800

    Updated Toast Handler
    -- added defaults

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -4,11 +4,11 @@
                 if (<?php echo json_encode(isset($_SESSION['_flash']['notification']) &&  count($_SESSION['_flash']['notification']) > 0); ?>) {
                     Toastify({
                         text: "<?php echo $_SESSION['_flash']['notification']['text'] ?? '' ?>",
-                        duration: <?php echo $_SESSION['_flash']['notification']['duration'] ?? '' ?>,
+                        duration: <?php echo $_SESSION['_flash']['notification']['duration'] ?? 3000 ?>,
                         close: true,
                         gravity: "bottom",
-                        position: "<?php echo $_SESSION['_flash']['notification']['position'] ?? '' ?>",
-                        backgroundColor: "<?php echo $_SESSION['_flash']['notification']['bg'] ?? '' ?>",
+                        position: "<?php echo $_SESSION['_flash']['notification']['position'] ?? 'center' ?>",
+                        backgroundColor: "<?php echo $_SESSION['_flash']['notification']['bg'] ?? 'blue' ?>",
                         stopOnFocus: true,
                         offset: {
                             y: '3rem'
