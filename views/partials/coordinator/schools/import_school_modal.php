024c089613353c45603c70efa32548ee6a9a0d2a
inicolas <na14an.07@gmail.com>
2024-09-06 00:36:22 +0800

    Update Import School Modal
    -- UI changes

--- views/partials/coordinator/schools/import_school_modal.php
+++ views/partials/coordinator/schools/import_school_modal.php
@@ -1,6 +1,6 @@
 <!-- Modal Button -->
 
-<button class="flex items-center w-fit shrink-0 px-3 py-2 rounded shadow-md bg-blue-600 text-white gap-2 font-bold" data-bs-toggle="modal" data-bs-target="#importSchoolModal">
+<button class="flex items-center w-fit shrink-0 px-3 py-2 rounded shadow-md bg-blue-500  text-white gap-2 font-bold hover:bg-blue-600" data-bs-toggle="modal" data-bs-target="#importSchoolModal">
     <i class="bi bi-upload"></i>
     <p>Import Schools</p>
 </button>
@@ -18,43 +18,29 @@
                     </div>
                     <button type="button" class="btn-close hover:text-red-500" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-
-                <div>
-                    Download and Fill-Up the Provided Form <br>
-                    <span style="font-size: 0.8em; color: red;">Upload the Provided Form Only</span>
+                <div class="flex items-center">
+                    <p>Fill-up the provided form <a href="/uploads/export_with_letterhead" class="text-blue-500 font-bold">here <i class="bi bi-download"></i></a></p>
                 </div>
-
-
-
-                <div class="modal-footer mt-4 flex justify-between items-center">
-                    <div class="flex items-center gap-2">
-                        <span>Download The School Form</span>
-                        <a href="/uploads/export_with_letterhead" style="color: blue;" class="btn p-0 text-blue-500 underline">HERE</a>
+                <form action="/coordinator/schools/importcsv" method="POST" enctype="multipart/form-data" class="w-full flex flex-col gap-3">
+                    Upload The Form Below:
+                    <input class="block file:px-2 file:py-1 text-zinc-500 border border-gray-300 rounded-lg cursor-pointer bg-zinc-50 focus:outline-none file:text-zinc-100 file:border-0 file:bg-[#434F72] file:text-foreground" name="uploadedForm" id="formFile" type="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-exce">
+                    <div class="modal-footer mt-4 w-full">
+                        <button type="button" class="btn font-bold text-[#000] hover:text-red-500 border-[1px] border-[#000] hover:border-red-500" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn font-bold text-white bg-blue-500 hover:bg-blue-400">Import</button>
                     </div>
-                    <form action="/coordinator/schools/importcsv" method="POST" enctype="multipart/form-data">
-                        Upload The Form Below:
-                        <div class="mb-3 flex items-center gap-2">
-                            <input class="form-control" type="file" id="formFile" name="uploadedForm">
-                            <button type="submit" class="btn font-bold text-white bg-blue-500 hover:bg-blue-400">Import</button>
-                        </div>
-                    </form>
+                </form>
+                <?php if (isset($errors['import_school']['uploadedForm'])): ?>
+                    <p class="error"><?= $errors['import_school']['uploadedForm'] ?></p>
+                <?php endif; ?>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        if (<?php echo json_encode(isset($errors['import_school']) &&  count($errors['import_school']) > 0); ?>) {
+                            var importSchoolModal = new bootstrap.Modal(document.getElementById('importSchoolModal'));
+                            importSchoolModal.show();
+                        }
+                    });
+                </script>
 
-                    <?php if (isset($errors['import_school']['uploadedForm'])): ?>
-                        <p class="error"><?= $errors['import_school']['uploadedForm'] ?></p>
-                    <?php endif; ?>
-                    <script>
-                        document.addEventListener('DOMContentLoaded', function() {
-                            if (<?php echo json_encode(isset($errors['import_school']) &&  count($errors['import_school']) > 0); ?>) {
-                                var importSchoolModal = new bootstrap.Modal(document.getElementById('importSchoolModal'));
-                                importSchoolModal.show();
-                            }
-                        });
-                    </script>
-
-                </div>
-                <div class="modal-footer mt-4">
-                    <button type="button" class="btn font-bold text-[#000] hover:text-red-500 border-[1px] border-[#000] hover:border-red-500" data-bs-dismiss="modal">Cancel</button>
-                </div>
             </div>
         </div>
 </main>
