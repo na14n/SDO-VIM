89234e4b1d0a3ca34311fccb0e9647ed1d27314a
JoeDelina <joeedmardelina1514@gmail.com>
2024-09-11 00:32:36 +0800

    Minor Revision

--- Http/controllers/custodian-resources/index.php
+++ Http/controllers/custodian-resources/index.php
@@ -30,6 +30,36 @@ $db = App::resolve(Database::class);
 
 $resources = [];
 
+$pagination = [
+    'pages_limit' => 10,
+    'pages_current' => isset($_GET['page']) ? (int)$_GET['page'] : 1,
+    'pages_total' => 0,
+    'start' => 0,
+];
+
+$resources_count = $db->query('SELECT COUNT(*) as total FROM school_inventory')->get();
+$pagination['pages_total'] = ceil($resources_count[0]['total'] / $pagination['pages_limit']);
+$pagination['pages_current'] = max(1, min($pagination['pages_current'], $pagination['pages_total']));
+
+$pagination['start'] = ($pagination['pages_current'] - 1) * $pagination['pages_limit'];
+
+$resources = $db->paginate('
+SELECT 
+    si.item_code,
+    si.item_article,
+    s.school_name,
+    si.item_status AS status,
+    si.date_acquired
+FROM 
+    school_inventory si
+LEFT JOIN 
+    schools s ON s.school_id = si.school_id
+LIMIT :start,:end
+', [
+    'start' => (int)$pagination['start'],
+    'end' => (int)$pagination['pages_limit'],
+])->get();
+
 $resources = $db->query('
     SELECT 
        si.item_code,
@@ -54,4 +84,5 @@ view('custodian-resources/index.view.php', [
     'statusMap' => $statusMap,
     'heading' => 'Resources',
     'resources' => $resources,
+    'pagination' => $pagination
 ]);
