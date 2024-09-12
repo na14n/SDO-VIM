bd5c1cf3c050020fcda7023c4f83773bf938fde7
Halcy0nin <155969605+Halcy0nin@users.noreply.github.com>
2024-09-12 22:46:27 +0800

    School Assigning bug fix

--- Http/controllers/resources/unassigned/patch.php
+++ Http/controllers/resources/unassigned/patch.php
@@ -1,28 +1,5 @@
 <?php
 
-//  ==========================================
-//           This is the Controller 
-// ===========================================
-// 
-//  This is where you load the corresponding
-//  view file for this route if available
-// 
-//   Use the view() function and feed the 
-//   full path of the view.
-// 
-//   Being the controller file. This is where 
-//   the data is get, manipulated, and/or
-//   saved.
-//      
-//   You can pass variables to your view as the
-//   second parameter of the view function.
-//      
-//   view('notes/{id}', ['notes' => $notes])
-//
-//   view variables are passed as keu-value
-//   pairs as illustrated in the example above.
-//
-
 use Core\Database;
 use Core\App;
 
@@ -31,37 +8,28 @@ $db = App::resolve(Database::class);
 $id = $_POST['id'];
 $item_code = $_POST['school_id'] . '-' . $id;
 
+// Update the school inventory
 $db->query('UPDATE school_inventory 
             SET school_id = :school_id, 
                 item_code = :new_item_code
-            WHERE item_code = :id;', [
-    'id' => $_POST['id'] ?? null,
+            WHERE item_code = :id', [
+    'id' => $id, 
     'new_item_code' => $item_code,
     'school_id' => $_POST['school_id']
 ]);
 
-$school_name = $db->query('
-SELECT
-    school_name
-FROM schools
-WHERE school_id = :school_id
+$school_name_query = $db->query('
+    SELECT school_name
+    FROM schools
+    WHERE school_id = :school_id
 ', [
     'school_id' => $_POST['school_id']
 ]);
 
-$custodian_id = $db->query('
-SELECT 
-    u.user_id
-FROM 
-    users u
-INNER JOIN 
-    schools s ON u.school_id = s.school_id
-WHERE 
-    s.school_id = :current_school_id;
-', [
-    'current_school_id' => $_POST['id_to_update']
-])->get();
+$school_name = $school_name_query->find();
 
-toast('Successfully allocated item ' . $_POST['id'] . ' to ' . $school_name . '.');
+$school_destination = $school_name['school_name'] ?? 'Unknown School';
+
+toast('Successfully allocated item ' . $_POST['id'] . ' to ' . $school_destination . '.');
 
 redirect('/coordinator/resources/unassigned');
