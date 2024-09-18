0b031c235318371a4f8a55493e8cfa2a4d6ff992
Halcy0nin <155969605+Halcy0nin@users.noreply.github.com>
2024-09-18 16:53:53 +0800

    remove export from pending, approved and denied tabs for users

--- views/users/approved/index.view.php
+++ views/users/approved/index.view.php
@@ -11,10 +11,6 @@ require base_path('views/partials/head.php') ?>
 <main class="main-col">
     <section class="flex items-center pr-12 gap-3">
         <?php require base_path('views/partials/banner.php') ?>
-        <button class="flex items-center w-fit shrink-0 px-3 py-2 rounded shadow-md bg-blue-500 text-white gap-2 font-bold">
-            <i class="bi bi-file-earmark-ruled-fill"></i>
-            <p>Export Users</p>
-        </button>
     </section>
     <section class="mx-12 mb-12 h-dvh rounded flex flex-col">
         <?php require base_path('views/partials/coordinator/users/tabs.php') ?>
